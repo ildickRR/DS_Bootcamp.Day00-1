b6a0223f6d604b9a55133107595b0369a98cdac4
ildickRR <mexmat307@mail.ru>
2025-10-25 22:42:02 +0300

    ex01

---
+++ ex01/json_to_csv.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+INPUT_FILE="hh.json"
+OUTPUT_FILE="hh.csv"
+
+{
+  echo '"id","created_at","name","has_test","alternate_url"'
+  jq -r -f filter.jq "$INPUT_FILE"
+} > "$OUTPUT_FILE"
+
+echo "Файл $OUTPUT_FILE успешно создан с заголовком."
